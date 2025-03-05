d1b7d2d4186c79859868298aa04f4e74e5790bbe
fugu2000 <9662506@graniteschools.org>
2025-03-05 12:01:54 -0700

    Update MadLibsUtility.java

--- src/MadLibsUtility.java
+++ src/MadLibsUtility.java
@@ -1,1 +1,23 @@
-
+public class MadLibsUtility {
+    public static String str;
+    public static boolean bInput;
+    public static void getValidString()
+    {
+        str = Main.scanner.nextLine();
+        while(str == " " || str == "")
+        {
+            System.out.println("PLEASE ENTER VALID INPUT");
+            str = Main.scanner.nextLine();
+        }
+    }
+    public static void getValidBoolean()
+    {
+        Main.boo.nextLine();
+        while(!Main.boo.hasNextBoolean())
+        {
+            System.out.println("PLEASE ENTER VALID INPUT");
+            Main.boo.nextLine();
+        }
+        Main.boo.nextBoolean();
+    }
+}
